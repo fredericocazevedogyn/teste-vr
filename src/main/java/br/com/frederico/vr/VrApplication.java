c8a3e377afbf508ea86e839d8f01d49d9ab17ceb
Frederico Carvalho de Almeida Azevedo <fredericocazevedo@gmail.com>
2025-07-08 15:24:40 -0300

    Criação do projeto

---
+++ src/main/java/br/com/frederico/vr/VrApplication.java
@@ -0,0 +1,13 @@
+package br.com.frederico.vr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/frederico/vr/VrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frederico.vr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
